171e5e58149c4514d63ecc636172e9965709d2b4
Enrique Pim <pimenrique96@gmail.com>
2026-05-10 04:07:57 +0300

    Updated code

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -1,17 +1,52 @@
 import React from "react";
 
-function ToyCard() {
+function ToyCard({ toy, onDeleteToy, onUpdateToy }) {
+  const { id, name, image, likes } = toy;
+
+  const handleDonate = () => {
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then(() => {
+        onDeleteToy(id);
+      });
+  };
+
+  const handleLike = () => {
+    const updateObj = {
+      likes: likes + 1,
+    };
+
+    fetch(`http://localhost:3001/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updateObj),
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => {
+        onUpdateToy(updatedToy);
+      });
+  };
+
   return (
     <div className="card" data-testid="toy-card">
-      <h2>{"" /* Toy's Name */}</h2>
+      <h2>{name}</h2>
       <img
-        src={"" /* Toy's Image */}
-        alt={"" /* Toy's Name */}
+        src={image}
+        alt={name}
         className="toy-avatar"
       />
-      <p>{"" /* Toy's Likes */} Likes </p>
-      <button className="like-btn">Like {"<3"}</button>
-      <button className="del-btn">Donate to GoodWill</button>
+      {/* Note the trailing space inside the paragraph to match the test's exact text string */}
+      <p>{likes} Likes </p>
+      <button className="like-btn" onClick={handleLike}>
+        Like {"<3"}
+      </button>
+      <button className="del-btn" onClick={handleDonate}>
+        Donate to GoodWill
+      </button>
     </div>
   );
 }
